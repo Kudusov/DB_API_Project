c3bee924972ffb43721d9be913f595ea70c4e821
Mahmud <qudusov98@mail.ru>
2018-03-12 15:32:25 +0300

    some change

--- db-project/src/main/java/dbproject/services/ThreadService.java
+++ db-project/src/main/java/dbproject/services/ThreadService.java
@@ -3,6 +3,10 @@ package dbproject.services;
 import dbproject.models.ThreadModel;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static dbproject.rowmappers.RowMappers.readThread;
 @Service
 public class ThreadService {
@@ -26,11 +30,6 @@ public class ThreadService {
                     "RETURNING id";
         }
 
-        final String updateThreadCount = "UPDATE Forums SET threads = threads + ? WHERE slug = ?";
-
-        final String getThreadById = "SELECT *, (SELECT nickname FROM users WHERE threads.user_id = id) as author," +
-                "  (SELECT slug FROM forums WHERE threads.forum_id = id) as forum FROM Threads where id = ?";
-
         final Integer id;
         if (thread.getCreated() != null) {
             /* добаляем thread*/
@@ -44,8 +43,49 @@ public class ThreadService {
                     thread.getSlug(), thread.getMessage(), thread.getTitle());
         }
         /* обновляем количество тредов в форуме */
-        jdbcTemplate.update(updateThreadCount, 1, forumSlug);
+        updateThreadCount(forumSlug, 1);
+        return getThreadById(id);
 
-        return jdbcTemplate.queryForObject(getThreadById, readThread, id);
+    }
+
+    public ThreadModel create2(ThreadModel thread, String forumSlug) {
+
+        final StringBuilder sqlCreate = new StringBuilder("UPDATE Users SET");
+        final List<Object> params = new ArrayList<>();
+
+        sqlCreate.append("INSERT INTO Threads (user_id, ");
+        params.add(thread.getAuthor());
+
+        if (thread.getCreated() != null) {
+            sqlCreate.append("created, ");
+            params.add(thread.getCreated());
+        }
+
+        sqlCreate.append("forum_id, slug, message, title) VALUES((SELECT id FROM Users WHERE nickname = ?), ");
+        if (thread.getCreated() != null) {
+            sqlCreate.append("?, ");
+        }
+
+        sqlCreate.append("(SELECT id FROM Forums WHERE slug = ?), ?, ?, ?) RETURNING id");
+        params.add(forumSlug);
+        params.add(thread.getSlug());
+        params.add(thread.getMessage());
+        params.add(thread.getTitle());
+
+        final Integer id = jdbcTemplate.queryForObject(sqlCreate.toString(), Integer.class, params.toArray());
+        updateThreadCount(forumSlug, 1);
+        return getThreadById(id);
+
+    }
+
+    public ThreadModel getThreadById(Integer id) {
+        final String sqlGetThreadById = "SELECT *, (SELECT nickname FROM users WHERE threads.user_id = id) as author," +
+                " (SELECT slug FROM forums WHERE threads.forum_id = id) as forum FROM Threads where id = ?";
+        return  jdbcTemplate.queryForObject(sqlGetThreadById, readThread, id);
+    }
+
+    public void updateThreadCount(String slug, Integer count) {
+        final String sqlUpdateThreadCount = "UPDATE Forums SET threads = threads + ? WHERE slug = ?";
+        jdbcTemplate.update(sqlUpdateThreadCount, count, slug);
     }
 }
